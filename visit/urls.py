526f72d3072c8ba1480370e3bf7b279e64ff09c8
Vinskao <tianyikao@gmail.com>
2025-06-11 11:17:12 +0800

    info: 在 visit/urls.py 中的路徑前加上 /voyeur 前綴

--- visit/urls.py
+++ visit/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
  
 urlpatterns = [
-    path('visit/count', views.get_visit_count, name='get_visit_count'),
-    path('visit/increment', views.increment_visit_count, name='increment_visit_count'),
+    path('voyeur/visit/count', views.get_visit_count, name='get_visit_count'),
+    path('voyeur/visit/increment', views.increment_visit_count, name='increment_visit_count'),
 ] 
